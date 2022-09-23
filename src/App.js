4c9a5e9986376e4414114592c2645dda0593c3a2
Hudson Thiago <hudsonthiagocorreia@gmail.com>
2022-09-23 00:30:33 -0300

    #3 - barra de pesquisa e tela de perfil do pokemon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Pokemons from './pages/Pokemons'
 import PokemonProfile from './pages/PokemonProfile'
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Pokemons />} />
-        <Route path='/pokemon/:pokemonId' element={<PokemonProfile/>} />
-      </Routes>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Pokemons />} />
+          <Route path='/pokemon/:pokemonId' element={<PokemonProfile/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
   );
 }
